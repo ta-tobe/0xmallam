e4dbf061c68305dda5ac94e26efc6c1ebc8cddb4
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-31 13:46:59 -0500

    Update index.ts

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,11 +1,4 @@
-import ada from './ada.svg'
-import avalanche from './avalanche.svg'
-import cosmos from './cosmos.svg'
-import eth from './eth.svg';
-import nano from './nano.svg';
-import polkadot from './polkadot.svg';
-import solana from './solana.svg';
-import xtz from './xtz.svg';
+import usd from './usd.svg';
 
 const icons: { [id: string]: string } = {
   ada,
