82e042caabc52093cf222c7b4062588a7e2a4110
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-31 11:43:31 -0500

    Update eth2.ts

--- data/eth2.ts
+++ data/eth2.ts
@@ -1,13 +1,7 @@
 import { IssuanceData } from './types'
 
 export async function getEth2Data(): Promise<IssuanceData> {
-//   const req2 = await fetch("https://beaconcha.in/api/v1/epoch/latest", {
-//     "headers": {
-//       "content-type": "application/json",
-//     },
-//   });
-//   const { data } = await req2.json();
-  
+
   const req = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
       "content-type": "application/json",
@@ -20,6 +14,5 @@ export async function getEth2Data(): Promise<IssuanceData> {
     name: 'United States Dollars (USD)',
     category: 'stable',
     sevenDayMA: price,
-//     oneDay: data.validatorscount,
   };
 }
