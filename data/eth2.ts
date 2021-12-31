dcfa3e5ad7a4dea120a8afaa9a63c8a1790415f2
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-31 11:58:16 -0500

    Update eth2.ts

--- data/eth2.ts
+++ data/eth2.ts
@@ -5,12 +5,14 @@ export async function getEth2Data(): Promise<IssuanceData> {
   const round = (number, decimalPlaces) => {
   const factorOfTen = Math.pow(10, decimalPlaces)
   return Math.round(number * factorOfTen) / factorOfTen
-}
+  };
+  
   const req = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
       "content-type": "application/json",
     },
   });
+  
   const { price } = await req.json();
 
   return {
