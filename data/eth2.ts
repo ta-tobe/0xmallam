16e83454c7c51a613095a0623c5b3716db467aed
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-31 11:24:52 -0500

    Update eth2.ts

--- data/eth2.ts
+++ data/eth2.ts
@@ -12,7 +12,7 @@ export async function getEth2Data(): Promise<IssuanceData> {
     id: 'eth',
     name: 'United States Dollars (USD)',
     category: 'stable',
-    sevenDayMA: data.validatorscount,
+    sevenDayMA: data.properslashingscount,
     oneDay: 74,
   };
 }
