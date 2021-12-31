d83cfdeb4f441f44ee34dd0fc05352449f74837d
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-31 13:47:14 -0500

    Update index.ts

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,14 +1,7 @@
 import usd from './usd.svg';
 
 const icons: { [id: string]: string } = {
-  ada,
-  avalanche,
-  eth,
-  nano,
-  polkadot,
-  cosmos,
-  solana,
-  xtz,
+  usd,
 }
 
 export default icons;
