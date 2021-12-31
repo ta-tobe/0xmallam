7f8d8a36f61666ed7a65a6fd3a71880c39471299
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-12-30 21:20:07 -0500

    Update List.tsx

--- components/List.tsx
+++ components/List.tsx
@@ -15,13 +15,9 @@ const List: React.FC<ListProps> = ({ data }) => {
   return (
     <div className="list">
       <div className="header">
-        <div className="name">Currency</div>
+        <div className="name">Name</div>
         <div className="amount">
           Bid
-        </div>
-<!--         <div className="ask">
-          Ask
-        </div> -->
       </div>
 
       {sortedData.map((protocol: IssuanceData) => (
