fb331c2a08a565b590794397ad4fc20901f9677e
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-01-01 02:00:50 -0500

    Update index.ts

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,7 +1,11 @@
 import usd from './usd.svg';
+import gbp from './gbp.svg';
+import eur from './eur.svg';
 
 const icons: { [id: string]: string } = {
   usd,
+  gbp,
+  eur,
 }
 
 export default icons;
