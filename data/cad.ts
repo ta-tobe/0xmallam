52d9e09e89dc785eae30d229c787f1bef880387e
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-01-03 06:03:17 -0500

    Update cad.ts

--- data/cad.ts
+++ data/cad.ts
@@ -1,19 +1,19 @@
 import { IssuanceData } from './types'
 
-export async function getEurData(): Promise<IssuanceData> {
+export async function getCadData(): Promise<IssuanceData> {
 
   const round = (number, decimalPlaces) => {
   const factorOfTen = Math.pow(10, decimalPlaces)
   return Math.round(number * factorOfTen) / factorOfTen
   };
   
-  const req = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=EURUSDT", {
-    "headers": {
-      "content-type": "application/json",
-    },
-  });
+//   const req = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=EURUSDT", {
+//     "headers": {
+//       "content-type": "application/json",
+//     },
+//   });
   
-  const { price } = await req.json();
+//   const { price } = await req.json();
 
   const req2 = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
@@ -24,8 +24,8 @@ export async function getEurData(): Promise<IssuanceData> {
   const data = await req2.json();
   
   return {
-    id: 'eur',
-    name: 'Euros (EUR)',
+    id: 'cad',
+    name: 'Canadian Dollars (CAD)',
     category: 'stable',
 <!--     rate: round(price * data.price,0), -->
       rate: round(0.79 * data.price,0),
