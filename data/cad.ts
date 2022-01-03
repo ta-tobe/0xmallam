16699ee3ee71e24f482beba34424049b5dd80a33
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-01-03 06:02:08 -0500

    Create cad.ts

---
+++ data/cad.ts
@@ -0,0 +1,34 @@
+import { IssuanceData } from './types'
+
+export async function getEurData(): Promise<IssuanceData> {
+
+  const round = (number, decimalPlaces) => {
+  const factorOfTen = Math.pow(10, decimalPlaces)
+  return Math.round(number * factorOfTen) / factorOfTen
+  };
+  
+  const req = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=EURUSDT", {
+    "headers": {
+      "content-type": "application/json",
+    },
+  });
+  
+  const { price } = await req.json();
+
+  const req2 = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
+    "headers": {
+      "content-type": "application/json",
+    },
+  });
+  
+  const data = await req2.json();
+  
+  return {
+    id: 'eur',
+    name: 'Euros (EUR)',
+    category: 'stable',
+<!--     rate: round(price * data.price,0), -->
+      rate: round(0.79 * data.price,0),
+
+  };
+}
