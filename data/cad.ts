357fa3bcd0e6eb87163eab5cc8f47c8c10ff788d
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-04-20 21:19:45 -0400

    Update cad.ts

--- data/cad.ts
+++ data/cad.ts
@@ -13,7 +13,7 @@ export async function getCadData(): Promise<IssuanceData> {
     },
   });
   
-  const data = await req.json();
+  const data1 = await req.json();
 
   const req2 = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
@@ -29,7 +29,7 @@ export async function getCadData(): Promise<IssuanceData> {
     id: 'cad',
     name: 'Canadian Dollars (CAD)',
     category: 'stable',
-    rate: round(data.cad-coin.usd * data.price,0),
+    rate: round(data1.cad-coin.usd * data.price,0),
 
   };
 }
