104b7170e0c975ba2bc74218e437380bb595ddc4
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-04-21 00:18:06 -0400

    Update cad.ts

--- data/cad.ts
+++ data/cad.ts
@@ -7,13 +7,13 @@ export async function getCadData(): Promise<IssuanceData> {
   return Math.round(number * factorOfTen) / factorOfTen
   };
   
-  const req = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=cad-coin&vs_currencies=usd", {
-    "headers": {
-      "content-type": "application/json",
-    },
-  });
+//   const req = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=cad-coin&vs_currencies=usd", {
+//     "headers": {
+//       "content-type": "application/json",
+//     },
+//   });
   
-  const {1} = await req.json();
+//   const {1} = await req.json();
 
   const req2 = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
@@ -29,7 +29,7 @@ export async function getCadData(): Promise<IssuanceData> {
     id: 'cad',
     name: 'Canadian Dollars (CAD)',
     category: 'stable',
-    rate: round(1.usd * data.price,0),
+    rate: round(0.79 * data.price,0),
 
   };
 }
