627a78420a7727acecf54002c828d9fb1a7ea121
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2022-11-08 10:59:32 -0500

    Update cad.ts

--- data/cad.ts
+++ data/cad.ts
@@ -8,12 +8,13 @@ export async function getCadData(): Promise<IssuanceData> {
   };
   
 //   const req = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=cad-coin&vs_currencies=usd", {
-//     "headers": {
-//       "content-type": "application/json",
-//     },
-//   });
+    const req = await fetch("https://min-api.cryptocompare.com/data/price?fsym=CAD&tsyms=USD", {
+    "headers": {
+      "content-type": "application/json",
+    },
+  });
   
-//   const {1} = await req.json();
+  const {CAD} = await req.json();
 
   const req2 = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=USDTNGN", {
     "headers": {
@@ -29,7 +30,7 @@ export async function getCadData(): Promise<IssuanceData> {
     id: 'cad',
     name: 'Canadian Dollars (CAD)',
     category: 'stable',
-    rate: round(0.79 * data.price,0),
+    rate: round(CAD * data.price,0),
 
   };
 }
